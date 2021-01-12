c44d977248f97e64303a8f6ab9c76b3d3a57e235
Naveen <naveen@pop-os.localdomain>
2021-01-12 19:07:14 +0530

    added login page designs

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <div id="nav">
-    <!-- <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> -->
-  </div>
   <router-view />
 </template>
 
@@ -13,5 +9,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0;
+  padding: 0;
 }
 </style>
